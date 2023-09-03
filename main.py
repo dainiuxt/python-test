ab1efced0e7a6124e324b346323514a0600a7b1d
dainiuxt <dainiuxt@gmail.com>
2023-09-03 20:43:22 +0300

    add final solution with extra task and comments

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def find_index_of_darkest_street_light(road_length: int, not_working_street_ligh
   
   illuminations = [] # illuminations for not working street lights
   below_one_illuminations_index = [] #index of street lights where illumination < 1
+  consecutives_below_one = []
   below_one_illuminations = []
   for val in not_working_street_lights:
     illumination = 0 # self illumination of not working street light
@@ -39,23 +40,51 @@ def find_index_of_darkest_street_light(road_length: int, not_working_street_ligh
       below_one_illuminations.append(illumination) # values of very dimm (<1) street light      
   
   # At least every 7th street light should be working to have cumulative illumination above 1. We should check if len(below_one_illuminations) > 14 then replace every 7th lamp (remove every 7th member from list and check again), if list length < 14 we can simply remove the midle number. Downsides are that we can have 15 inactive lamps in a row, then some singles or 2-3, hence index consistency check should be implemented.
-  
-  print("Below 1 illuminations indexes:")
-  print(below_one_illuminations_index)
-  print("Below one illumination values:")
-  print(below_one_illuminations)
+
+  # Below code builds the list of consecutives list from non_working and illuminations_below_one (code borrowed from https://softhints.com/find-consecutive-numbers-in-a-list-in-python/, I chose vanilla python version at the end of article.)
+  if len(below_one_illuminations) > 14:  
+    current_consecutive = [below_one_illuminations_index[0]]
+    for i in range(1, len(below_one_illuminations_index)):
+      if below_one_illuminations_index[i] == below_one_illuminations_index[i-1] + 1:
+          current_consecutive.append(below_one_illuminations_index[i])
+      else:
+          if len(current_consecutive) > 1:
+              consecutives_below_one.append(current_consecutive)
+          current_consecutive = [below_one_illuminations_index[i]]
+    if len(current_consecutive) > 1:
+      consecutives_below_one.append(current_consecutive)
+  # END of borrowerd code.
+
+  # At least every 7th member of long inactive sequence should be replaced `len(consecutives_below_one[i])/7`, to keep illumination above 1.
+  should_be_brightened = []
+  for i in range(0, len(consecutives_below_one)):
+    if len(consecutives_below_one[i]) > 7:
+      j = 3
+      while j < len(consecutives_below_one[i]):
+        should_be_brightened.append(consecutives_below_one[i][j])
+        j = j + 7
+ 
+  # Debug prints. Will be removed later.
+  # print("All consecutives below one:")
+  # print(consecutives_below_one)
+  # print("Below 1 illuminations indexes:")
+  # print(below_one_illuminations_index)
+  # print("Below one illumination values:")
+  # print(below_one_illuminations)
+  # print("Lamps should be replaced to keep illuminations above 1:")
   
   dimmest_set = []
   dimmest = illuminations.index(min(illuminations))
   dimmest_set.append(dimmest)
+  
+  print(f"Lamps on lightposts No. {should_be_brightened} should be changed to keep illumination at least 1.")
+  print(f"Total quantity of lamps should be changed to keep illumination at least 1: {len(should_be_brightened)}.")
+  print(f"The dimmest lamp for change is No. {not_working_street_lights[dimmest_set[0]]}")
+  
+  # Row below returns the dimmest lamp post index starting to count from 0 for testing.
   return not_working_street_lights[dimmest_set[0]]
 
-
-
-
-find_index_of_darkest_street_light(road_length=20000, not_working_street_lights=[101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 116, 117, 118, 119, 120, 121, 123, 124, 125])
-
-# find_index_of_darkest_street_light(road_length=20000, not_working_street_lights=[101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125])
+find_index_of_darkest_street_light(road_length=200000, not_working_street_lights=[4, 5, 6, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217])
 
 if __name__ == "__main__":
     # This is an example test. When evaluating the task, more will be added:
